4352b38d3cca911e24db19aa3f42097d434c3180
Mihir Mehta <mihirthegenius@gmail.com>
2014-12-01 01:37:24 -0600

    Fixing positional parameters in ta_wrapper.sh

--- ta_wrapper.sh
+++ ta_wrapper.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
-calc < $1 > /dev/null 2> /dev/null
-mv /tmp/lts.dot /tmp/lts1.dot
-v1=`head -n 2 /tmp/lts1.dot | tail -n 1 | sed 's/;$//'`
-calc < $2 > /dev/null 2> /dev/null
-mv /tmp/lts.dot /tmp/lts2.dot
-v2=`head -n 2 /tmp/lts2.dot | tail -n 1 | sed 's/;$//'`
-bash -c "relts --lts1 /tmp/lts1.dot --lts2 /tmp/lts2.dot -p $v1 -q $v2 -n 0"
+
+MINPARAMS=2
+if [ $# -lt "$MINPARAMS" ]
+then
+    echo
+    echo "This script needs at least $MINPARAMS command-line arguments for ta1 and ta2!"
+else
+    
+    calc < $1 > /dev/null 2> /dev/null
+    mv /tmp/lts.dot /tmp/lts1.dot
+    v1=`head -n 2 /tmp/lts1.dot | tail -n 1 | sed 's/;$//'`
+    calc < $2 > /dev/null 2> /dev/null
+    mv /tmp/lts.dot /tmp/lts2.dot
+    v2=`head -n 2 /tmp/lts2.dot | tail -n 1 | sed 's/;$//'`
+    if [ -n "$3"]
+    then
+        if [ -n "$4"]
+        then
+            bash -c "relts --lts1 /tmp/lts1.dot --lts2 /tmp/lts2.dot -p $v1 -q $v2 -n $3 -k $4"
+        else
+            bash -c "relts --lts1 /tmp/lts1.dot --lts2 /tmp/lts2.dot -p $v1 -q $v2 -n $3"
+        fi
+    else
+        if [ -n "$4"]
+        then
+            bash -c "relts --lts1 /tmp/lts1.dot --lts2 /tmp/lts2.dot -p $v1 -q $v2 -k $4"
+        else
+            bash -c "relts --lts1 /tmp/lts1.dot --lts2 /tmp/lts2.dot -p $v1 -q $v2"
+        fi
+    fi
+fi  
